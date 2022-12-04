922a6b5902d2a239863139a8d8c281d9dd2d84b7
BeardedPlatypus <mwtegelaers@gmail.com>
2022-12-04 11:17:58 -0500

    Add fsharp exercise 3.1

--- fsharp/advent/advent/Program.fs
+++ fsharp/advent/advent/Program.fs
@@ -3,4 +3,5 @@
 printfn $"Challenge 1.1: {day_1.calculate(1)}"
 printfn $"Challenge 1.2: {day_1.calculate(3)}"
 printfn $"Challenge 2.1: {day_2.calculate(day_2.Part.One)}"
-printfn $"Challenge 2.1: {day_2.calculate(day_2.Part.Two)}"
+printfn $"Challenge 2.2: {day_2.calculate(day_2.Part.Two)}"
+printfn $"Challenge 3.1: {day_3.calculate()}"
---
+++ fsharp/advent/advent/challenges/day_3.fs
@@ -0,0 +1,33 @@
+﻿namespace BeardedPlatypus.advent.challenges
+
+open BeardedPlatypus.advent.common
+
+module internal day_3 =
+    type Element = Set<char> list
+
+    let priority_mapping : Map<char, int> = 
+        Map <| (Seq.mapi (fun i c -> (c, i+1)) "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+    let get_priority (v: char) : int =
+        priority_mapping |> Map.find v
+
+    let private preprocess_part1 (lines: string seq) : Element seq =
+        let mapper (l: string) : Element =
+            let split: int = l.Length / 2
+            [Set.ofSeq l[..(split-1)]; Set.ofSeq l[split..]]
+
+        Seq.map mapper lines
+
+    let private compute (elements: Element seq) : int =
+        let mapper (elem: Element) : int =
+            Set.intersectMany elem 
+            |> Set.minElement  // At this point the data should only contain a single element
+            |> get_priority
+
+        Seq.sum <| Seq.map mapper elements
+
+    let public calculate () : int =
+        file_utils.read_input_file "day_03.txt"
+        |> preprocess_part1
+        |> compute
+
