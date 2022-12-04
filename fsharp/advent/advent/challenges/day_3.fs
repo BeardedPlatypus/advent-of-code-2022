adea4fe9d2ee9506001eda8348ef980217ab2915
BeardedPlatypus <mwtegelaers@gmail.com>
2022-12-04 11:27:14 -0500

    Add fsharp exercise 3.2

--- fsharp/advent/advent/challenges/day_3.fs
+++ fsharp/advent/advent/challenges/day_3.fs
@@ -3,7 +3,7 @@
 open BeardedPlatypus.advent.common
 
 module internal day_3 =
-    type Element = Set<char> list
+    type Element = Set<char> seq
 
     let priority_mapping : Map<char, int> = 
         Map <| (Seq.mapi (fun i c -> (c, i+1)) "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
@@ -14,9 +14,16 @@ module internal day_3 =
     let private preprocess_part1 (lines: string seq) : Element seq =
         let mapper (l: string) : Element =
             let split: int = l.Length / 2
-            [Set.ofSeq l[..(split-1)]; Set.ofSeq l[split..]]
+            seq { Set.ofSeq l[..(split-1)]; Set.ofSeq l[split..] }
 
         Seq.map mapper lines
+
+    let private preprocess_part2 (n: int) (lines: string seq) : Element seq =
+        lines
+        |> Seq.map Set.ofSeq
+        |> Seq.indexed
+        |> Seq.groupBy (fun (i, _) -> i / n)
+        |> Seq.map (fun (_, v) -> v |> Seq.map (fun (_, s) -> s) )
 
     let private compute (elements: Element seq) : int =
         let mapper (elem: Element) : int =
@@ -26,8 +33,18 @@ module internal day_3 =
 
         Seq.sum <| Seq.map mapper elements
 
-    let public calculate () : int =
+    [<RequireQualifiedAccess>]
+    type public Part =
+         | One
+         | Two
+
+    let public calculate (part: Part) : int =
+        let preprocess = 
+            match part with
+            | Part.One -> preprocess_part1
+            | Part.Two -> preprocess_part2 3
+
         file_utils.read_input_file "day_03.txt"
-        |> preprocess_part1
+        |> preprocess
         |> compute
 
